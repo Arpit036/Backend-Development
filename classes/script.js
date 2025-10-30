9020f44bf05c46c92266a61eb03cb0ec9e234ea1
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-10-30 21:23:27 +0530

    express syntax

--- classes/script.js
+++ classes/script.js
@@ -27,12 +27,26 @@
 // })
 
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer(function(req, res){
-    res.end("hello world");
+// const server = http.createServer(function(req, res){
+//     res.end("hello world");
+// })
+
+// server.listen(3000);
+
+
+
+
+
+//  express .js
+const express = require('express')
+const app = express();
+
+app.get('/', function(req, res){
+    res.send("hello world")
 })
 
-server.listen(3000);
+app.listen(3000)
 
  
