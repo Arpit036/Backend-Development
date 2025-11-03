ffc89520929e6ce903e88d28722494105e261eda
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-03 14:20:14 +0530

    add code

--- classes/script.js
+++ classes/script.js
@@ -1,68 +1,17 @@
-// const fs = require('fs')
-
-// fs.writeFile('hey.txt', "hey hello kaise ho", function(err) {
-//    if(err) console.log(err);
-//     else console.log("done");
-// })
-
-// fs.appendFile('hey.txt', " mai to accha hun", function(err) {
-//    if(err) console.log(err);
-//     else console.log("done");
-// })
-
-
-// fs.rename("hey.txt", "hello.txt", function(err){
-//     if(err) console.log(err)
-//         else console.log("done")
-// })
-
-// fs.copyFile("hello.txt", "./copy/copy.txt", function(err){
-//     if(err) console.log(err)
-//         else console.log(done)
-// })
-
-// fs.unlink("hello.txt", function(err){
-//     if(err) console.log(err)
-//         else console.log(removed)
-// })
-
-
-// const http = require('http');
-
-// const server = http.createServer(function(req, res){
-//     res.end("hello world");
-// })
-
-// server.listen(3000);
-
-
-
-
-
-//  express .js
 const express = require('express')
 const app = express();
+const path = require('path');
 
- 
+app.set("view engine", "ejs");
 app.use(express.json())
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 
 app.get('/', function(req, res){
-    res.send("hello world");
+    res.render("index");
+
 })
 
-app.get('/profile', function(req, res){
-     res.send("profile section")
-});
-
-app.use((err, req, res, next) => {
-    console.log(err.stack)
-    res.status(500).send('Something broke')
-});
-
-app.listen(3000)
-
-
-
- 
+app.listen(3000);
