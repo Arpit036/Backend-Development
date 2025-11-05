b0c5a9ec890a488534ccf0853892e5c6c898ce46
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-05 21:01:27 +0530

    final set

--- Class_CRUD/app.js
+++ Class_CRUD/app.js
@@ -6,9 +6,22 @@ const userModel = require('./usermodel');
 app.get('/', function(req, res){
     res.send("hey");
 })
+ 
 
 app.get('/create', function(req, res){
-    res.send("hey");
+     const createdUser = userModel.create({
+        name : "Arpit",
+        email: "arpitji784@gmail.com",
+        username: "arpit_036"
+
+     })
+
+     
+    res.send(createdUser);
 })
+
+
+
+
 
 app.listen(3000);
--- Class_CRUD/usermodel.js
+++ Class_CRUD/usermodel.js
@@ -1,9 +1,22 @@
-// require mongoose
+ // require mongoose
 const mongoose = require('mongoose')
 
-// connect with mongoDB 
-mongoose.connect(`mongodb://127.0.0.1:27017/name`)
+// connect with mongoDB '
 
+async function connectDB(){
+    
+ try{
+     await mongoose.connect("mongodb://127.0.0.1:27017/myDatabase", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+     });
+     console.log("mongoDB connected successfully !")
+    }
+    catch(err){
+        console.log("failed", err);
+    }
+}
+connectDB();
 
 // create Schema 
 const userSchema = mongoose.Schema({
@@ -12,23 +25,7 @@ const userSchema = mongoose.Schema({
     email: String
 })
 
-// model ke basis pr hi CRUD kr skte ho.
-// so we need model 
-// given name ka pluralised version name ka model bnega . ex : user -> users
-
-// mongoose.model("user", userSchema);
+ 
+ 
 
 module.exports = mongoose.model("user", userSchema);  
-
-// and isme name(user) ke sath ye bhi btana hota hai ki schema kaun sa use krna hai. so hm userSchema use kr rhe hain jo upr bnaya hai.
-
-
-// CRUD operation 
-// now we need crud operation on the respectively way(Routes) -> /register, /user , /update, /delete
-
-//  Hm log ye sare kaam routes ki help se krna chahte hain, jiske liye hme model export krna hi pdega
-
-/*  module.exports = mongoose.model("user", userSchema);  */
-
-
-// and now we have a file : usermodel.js jise hm application.js mre require krna hai . now do that. 
