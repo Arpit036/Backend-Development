cce478e1c8d0970eadd6594a2b0a8ca816cafe04
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-05 20:46:52 +0530

    almost complete all code before creating usermodel

--- CRUD(EJS)/app.js
+++ CRUD(EJS)/app.js
@@ -11,5 +11,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res)=>{
     res.render("index");
 })
+// app.get('/create', (req, res)=>{
+//     res.render("index");
+// })
+
+
+
+app.get('/read', (req, res)=>{
+    res.render("read");
+})
+app.post('/create', (req, res)=>{
+   
+})
+// app.get('/update', (req, res)=>{
+//     res.render("index");
+// })
+// app.get('/delete', (req, res)=>{
+//     res.render("index");
+// })
 
 app.listen(3000);
