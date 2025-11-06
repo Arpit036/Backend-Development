9df848f0f196ff29036e0faf067531d9a2049300
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-07 00:23:59 +0530

    JWT

--- AuthticationAutherization/app.js
+++ AuthticationAutherization/app.js
@@ -1,19 +1,24 @@
-// Bcrypt
-
+// JWT
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
- 
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser());
 
 app.get("/", function(req, res){
-     bcrypt.genSalt(10, function(err, salt){
-        bcrypt.hash("ewifwiownok", salt, function(err, hash){
-            // store hash in your password DB.
-            console.log(hash);
-            
-        })
-     })
+     let token =  jwt.sign({email: "harsh@example.com"}, "secret")
+     res.cookie("token", token);
+     console.log("done");
+         
+    })
+
+app.get("read", function(req, res){
+     let data = jwt.verify(req.cookies.token, "secret");
+     console.log(data)
 })
+    
 
  
 app.listen(3000);
