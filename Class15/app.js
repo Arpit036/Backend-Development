a66f054f2de0482757ed9f3d63391f3744249e77
Arpit  Kumar Verma <132037701+Arpit036@users.noreply.github.com>
2025-11-07 18:50:58 +0530

    complete signup and login

--- Class15/app.js
+++ Class15/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const userModel = require("./models/user")
 
 const cookieParser = require('cookie-parser');
 const path = require('path')
-
+const bcrypt= require('bcrypt');
+const { log } = require('console');
+const jwt = require('jsonwebtoken')
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -14,5 +17,54 @@ app.use(cookieParser());
 app.get('/', (req, res)=> {
     res.render('index')
 })
+
+app.post('/create',  (req, res)=> {
+    let {username, email, password, age} = req.body;
+  
+    bcrypt.genSalt(10, (err, salt)=>{
+        bcrypt.hash(password, salt, async (err, hash)=>{
+             let createdUser= await userModel.create({
+                username,
+                email,
+                password : hash,
+                age
+             })
+
+             let token = jwt.sign({email}, "shhhhhh");
+             res.cookie("token", token);
+             res.send(createdUser);
+        })
+    })
+})
+
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+app.post('/login', async(req, res)=>{
+    let user = await userModel.findOne({email: req.body.email})
+     if(!user) return res.send("something went wrong");
+     
+     bcrypt.compare(req.body.password, user.password, function(err, result){
+         if(result){
+            let token = jwt.sign({email: user.email}, "shhhhhh");
+             res.cookie("token", token);
+             res.send("yes you can login");
+            } 
+         else res.send("no you cant login")
+        
+     })
+
+});
+
+app.get("/logout", function(req, res){
+   res.cookie("token", "");
+   res.redirect("/");
+
+})
+
+
+
+    
+    
 
 app.listen(3000);
